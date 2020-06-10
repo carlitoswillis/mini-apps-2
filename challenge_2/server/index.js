697626e74ccd982cfb52a0d66f91ab71f5265d07
carlitoswillis <carlitoswillis@berkeley.edu>
2020-06-10 12:00:13 -0700

    set up server

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+app.use(express.json());
+app.use('/', express.static('public'));
+const port = 3000;
 
 
+app.listen(port, () => {
+  console.log(`listening at http://${os.networkInterfaces().lo0[0].address}:${port}`);
+});
